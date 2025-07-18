2009178faffbfd6d2b993a1cb84cc42dfd72ac22
Kouji Matsui <k@kekyo.net>
2025-07-18 13:15:42 +0900

    refactor: Refactored project structure.

--- demo/vite.config.mjs
+++ demo/vite.config.mjs
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import typedMessage from '../dist/vite-plugin.mjs';
-import { resolve } from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import typedMessage from 'typed-message/vite';
 
 export default defineConfig({
   plugins: [
@@ -20,9 +14,8 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  resolve: {
-    alias: {
-      'typed-message': resolve(__dirname, '../dist/index.mjs'),
-    },
-  },
+  build: {
+    rollupOptions: {
+    }
+  }
 });
